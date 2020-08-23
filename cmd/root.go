cc266a18a84ffea3c8fc7e570dd4c764cb831f21
Alex Korzh <alexkorzh7@gmail.com>
2020-08-23 20:53:27 +0200

    Make app follow CLI file structure convention

---
+++ cmd/root.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/AlexKLWS/youtube-audio-stream/client"
+	"github.com/AlexKLWS/youtube-audio-stream/downloader"
+	"github.com/spf13/cobra"
+)
+
+var (
+	outputFilename string
+	rootCmd        = &cobra.Command{
+		Use:   "youtube-audio-stream",
+		Short: "Runs the server",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			run(args[0])
+		},
+	}
+)
+
+func init() {
+	rootCmd.Flags().StringVarP(&outputFilename, "output", "o", "", "specify output filename")
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%+v", err)
+		os.Exit(1)
+	}
+}
+
+func run(url string) {
+	httpTransport := &http.Transport{
+		IdleConnTimeout:       60 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+	httpTransport.DialContext = (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}).DialContext
+
+	c := client.NewSilent(httpTransport)
+	ctx := context.Background()
+
+	d := downloader.New(c, url)
+	d.DownloadVideo(ctx, outputFilename)
+}
--- main.go
+++ main.go
@@ -1,50 +1,8 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"net"
-	"net/http"
-	"os"
-	"time"
-
-	"github.com/AlexKLWS/youtube-audio-stream/client"
-	"github.com/AlexKLWS/youtube-audio-stream/downloader"
-	"github.com/spf13/cobra"
-)
-
-// the command to run the server
-var rootCmd = &cobra.Command{
-	Use:   "youtube-audio-stream",
-	Short: "Runs the server",
-	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		run(args[0])
-	},
-}
+import "github.com/AlexKLWS/youtube-audio-stream/cmd"
 
 func main() {
 	// config.InitializeViper()
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "%+v", err)
-		os.Exit(1)
-	}
-}
-
-func run(url string) {
-	httpTransport := &http.Transport{
-		IdleConnTimeout:       60 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-	}
-	httpTransport.DialContext = (&net.Dialer{
-		Timeout:   30 * time.Second,
-		KeepAlive: 30 * time.Second,
-	}).DialContext
-
-	c := client.New(httpTransport)
-	ctx := context.Background()
-
-	d := downloader.New(c, url)
-	d.DownloadVideo(ctx, "")
+	cmd.Execute()
 }
