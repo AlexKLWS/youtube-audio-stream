812a2080cfe6e997c2dbecc1b76e189a5f7233b3
Alex Korzh <alexkorzh7@gmail.com>
2020-08-26 23:31:32 +0200

    Add simple router

---
+++ router/router.go
@@ -0,0 +1,51 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/AlexKLWS/youtube-audio-stream/consts"
+	"github.com/spf13/viper"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+// Router struct
+type Router struct {
+	Server    *echo.Echo
+	Videos    *echo.Group
+	Playlists *echo.Group
+}
+
+// New echo router
+func New() *Router {
+	e := echo.New()
+
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Root:   "../client/build",
+		HTML5:  true,
+		Browse: false,
+	}))
+	e.Use(middleware.Recover())
+
+	if viper.GetBool(consts.Debug) {
+		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowCredentials: true,
+			AllowOrigins:     []string{"http://localhost:3000"},
+			AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+		}))
+	}
+
+	// Serving the
+	e.Static("/output", "./output")
+	// Serving the website
+	e.Static("/", "../client/build")
+
+	a := e.Group("/api")
+
+	return &Router{
+		Server:    e,
+		Videos:    a.Group("/videos"),
+		Playlists: a.Group("/playlists"),
+	}
+}
