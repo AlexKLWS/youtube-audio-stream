fc7d156ec3c8ad5d373a00a1cd71f75d2c984933
Alex Korzh <alexkorzh7@gmail.com>
2020-09-06 00:08:06 +0200

    Add function that gets source file path

---
+++ files/source_path.go
@@ -0,0 +1,23 @@
+package files
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/AlexKLWS/youtube-audio-stream/consts"
+	"github.com/spf13/viper"
+)
+
+func GetSourceFilePath(videoID string) (string, error) {
+	path := filepath.Join(viper.GetString(consts.SourceDir), videoID)
+	var file string
+	err := filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+		if info.Name() != viper.GetString(consts.CompletionMarker) && !info.IsDir() {
+			// We assume there're only 2 files in a folder
+			file = info.Name()
+		}
+		return nil
+	})
+	path = filepath.Join(path, file)
+	return path, err
+}
